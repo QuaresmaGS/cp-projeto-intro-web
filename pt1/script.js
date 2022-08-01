8f098504ff245a9f6781c8834dd8112db9228ce4
Gabriel <gabrielssquaresma@gmail.com>
2022-08-01 15:35:10 -0300

    refresh: second week of intro to the web

--- pt1/script.js
+++ pt1/script.js
@@ -6,26 +6,26 @@ const obras = undefined
 const nome1 = "ludwig von mises";
 const idade1 = 92;
 const bom1 = true;
-const obras1 = ['-Socialismo: Uma Análise Econômica e Sociológica\n-A Mentalidade Anticapitalista\n-The Theory of Money and Credit'];
+const obras1 = ["Socialismo: Uma Análise Econômica e Sociológica", "A Mentalidade Anticapitalista", "The Theory of Money and Credit"];
 
 const nome2 = "Friedrich August von Hayek";
 const idade2 = 92;
 const bom2 = true;
-const obras2 = ['\n-O Caminho da Servidão\n-Os Fundamentos da Liberdade \n-Desestatização do dinheiro'];
+const obras2 = ["O Caminho da Servidão", "Os Fundamentos da Liberdade", "Desestatização do dinheiro"];
 
 const nome3 = "Adam Smith";
 const idade3 = 67;
 const bom3 = true;
-const obras3 = ['\n-A mão invisível\n-A Riqueza das Nações'];
+const obras3 = ["A mão invisível", "A Riqueza das Nações"];
 
 
-const ludwigVonMises = (`nome: ${nome1.toUpperCase()}: \nIdade: ${idade1} \nTem boas óbras?: ${bom1} \nObras:${obras1}`);
+const ludwigVonMises = (`nome: ${nome1.toUpperCase()}: \nIdade: ${idade1} \nTem boas óbras?: ${bom1} \nObras:\n-${obras1[0]} \n-${obras1[1]} \n-${obras1[2]} `);
 console.log(ludwigVonMises);
 
-const friedrichHayek = (`nome: ${nome2.toUpperCase()}: \nIdade: ${idade2} \nTem boas óbras?: ${bom2} \nObras:${obras2}`);
+const friedrichHayek = (`nome: ${nome2.toUpperCase()}: \nIdade: ${idade2} \nTem boas óbras?: ${bom2} \nObras:\n-${obras2[0]} \n-${obras2[1]} \n-${obras2[2]}`);
 console.log(friedrichHayek);
 
-const adamSmith = (`nome: ${nome3.toUpperCase()}: \nIdade: ${idade3} \nTem boas óbras?: ${bom3} \nObras:${obras2}`);
+const adamSmith = (`nome: ${nome3.toUpperCase()}: \nIdade: ${idade3} \nTem boas óbras?: ${bom3} \nObras:\n-${obras3[0]} \n-${obras3[1]}`);
 console.log(adamSmith);
 
 const verificação = bom1 && bom2 && bom3;
