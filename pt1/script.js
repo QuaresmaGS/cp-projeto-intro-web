01a376b81aaf2fb1f847f609d7cfbc3f338f5256
Gabriel <gabrielssquaresma@gmail.com>
2022-08-07 15:04:07 -0300

    semana 3

--- pt1/script.js
+++ pt1/script.js
@@ -3,23 +3,32 @@ const idade = Number;
 const bom = Boolean;
 const obras = undefined
 
-const nome1 = "ludwig von mises";
-const idade1 = 92;
-const bom1 = true;
-const obras1 = ['-Socialismo: Uma Análise Econômica e Sociológica\n-A Mentalidade Anticapitalista\n-The Theory of Money and Credit'];
+registro1 = {
+    nome1: "ludwig von mises",
+    idade1: 92,
+    bom1: true,
+    obras1: ['-Socialismo: Uma Análise Econômica e Sociológica\n-A Mentalidade Anticapitalista\n-The Theory of Money and Credit']
+};
 
-const nome2 = "Friedrich August von Hayek";
-const idade2 = 92;
-const bom2 = true;
-const obras2 = ['\n-O Caminho da Servidão\n-Os Fundamentos da Liberdade \n-Desestatização do dinheiro'];
+registro2 = {
+    nome2: "Friedrich August von Hayek",
+    idade2: 92,
+    bom2: true,
+    obras2: ['\n-O Caminho da Servidão\n-Os Fundamentos da Liberdade \n-Desestatização do dinheiro']
+};
 
-const nome3 = "Adam Smith";
-const idade3 = 67;
-const bom3 = true;
-const obras3 = ['\n-A mão invisível\n-A Riqueza das Nações'];
+registro3 = {
+    nome3: "Adam Smith",
+    idade3: 67,
+    bom3: true,
+    obras3: ['\n-A mão invisível\n-A Riqueza das Nações']
+};
 
+const registrosGerais = [];
 
-const ludwigVonMises = (`nome: ${nome1.toUpperCase()}: \nIdade: ${idade1} \nTem boas óbras?: ${bom1} \nObras:${obras1}`);
+registrosGerais.push(registro1, registro2, registro3)
+
+/*const ludwigVonMises = (`nome: ${nome1.toUpperCase()}: \nIdade: ${idade1} \nTem boas óbras?: ${bom1} \nObras:${obras1}`);
 console.log(ludwigVonMises);
 
 const friedrichHayek = (`nome: ${nome2.toUpperCase()}: \nIdade: ${idade2} \nTem boas óbras?: ${bom2} \nObras:${obras2}`);
@@ -32,4 +41,6 @@ const verificação = bom1 && bom2 && bom3;
 console.log(verificação);
 
 const media = (idade1+idade2+idade3)/3;
-const valorDaSoma = console.log(media);
+const valorDaSoma = console.log(media);*/
+
+console.log(registrosGerais);
